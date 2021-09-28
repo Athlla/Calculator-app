39b89edf4bd05a90ce2bac6cb8d8cb0274a45e81
Athlla <luthfi.thilla@gmail.com>
2021-09-28 11:38:19 +0700

    Fix keypad size on mobile view and create Display component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Keypad from './components/Keypad/Keypad';
+import Display from 'components/Display/Display';
+import Keypad from 'components/Keypad/Keypad';
 
-import styles from './styles/App.module.scss';
+import styles from 'styles/App.module.scss';
 
 const App = () => {
   return (
     <main className={styles.Container}>
+      <Display />
       <Keypad />
     </main>
   );
---
+++ src/components/Display/Display.tsx
@@ -0,0 +1,11 @@
+import styles from './Display.module.scss';
+
+const Display = () => {
+  return (
+    <div className={styles.Display}>
+      <span className={styles.Result}>123</span>
+    </div>
+  );
+};
+
+export default Display;
